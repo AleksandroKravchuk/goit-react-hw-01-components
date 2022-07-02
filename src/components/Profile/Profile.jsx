86596787b3714c824d328f765b33335152951d7c
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-07-02 21:07:07 +0300

    1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -36,7 +36,7 @@ Profile.propTypes = {
   tag: PropTypes.string,
   location:PropTypes.string,
   avatar: PropTypes.string,
-  stats: PropTypes.shape({
+  stats: PropTypes.exact({
   followers: PropTypes.number,
   views:PropTypes.number,
   likes: PropTypes.number,})
